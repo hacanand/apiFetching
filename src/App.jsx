a9f7fe9e6fc76da48e27520bc02cf016ef8b1cdb
anand <anandpromax@gmail.com>
2024-06-06 14:28:28 +0530

    project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css
+import MaterialTable from "material-table";
+
+function App() {
+  return (
+    
+    <div>
+      <MaterialTable
+      
+       </div>
+  );
+}
+
+export default App
