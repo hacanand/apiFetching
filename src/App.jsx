9db304f076e290de89b5768db30d6d4ec563f232
anand <anandpromax@gmail.com>
2024-06-06 14:44:15 +0530

    chore: Update dependencies and add Material Icons stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-// import './App.css
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import MaterialTable from "material-table";
 
 function App() {
+  // const [data, setData] = useState([]);
+  // const columns = [
+  //   { title: "ID", field: "id" },
+  //   { title: "Username", field: "username" },
+  //   { title: "Name", field: "name" },
+  //   { title: "Email", field: "email" },
+  //   { title: "Phone", field: "phone" },
+  //   { title: "Web Link", field: "website" },
+  // ];
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((resp) => resp.json())
+  //     .then((resp) => {
+  //       setData(resp);
+  //     });
+  // }, []);
+
   return (
-    
-    <div>
-      <MaterialTable
+    <div className="App ">
+       <h1 align="center">React-App</h1>
+      <h4 align="center">Material Table</h4>
+       <MaterialTable title="Employee Data" data={data} columns={columns} />
       
-       </div>
+    </div>
   );
 }
 
-export default App
+export default App;
